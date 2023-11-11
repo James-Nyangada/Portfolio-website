71abfc9d28d96912f9b6e2adcc42e54489c7f3e7
James Nyangada <jamesedwin03@gmail.com>
2023-11-11 19:52:53 +0300

    Version 2.1.2

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_eb6n7nc', 'template_emkhia5', form.current, 'WKQI1AZALcoUj2hiX')
+    emailjs.sendForm('service_ahoucsj', 'template_s3tlyuc', form.current, 'WKQI1AZALcoUj2hiX')
 
     e.target.reset()
     
@@ -60,7 +60,7 @@ const Contact = () => {
         {/* END OF CONTACT OPTIONS */}
         <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required />
-          <input type="email" name='Email' placeholder='Your Email' required />
+          <input type="email" name='email' placeholder='Your Email' required />
           <textarea name="message"  rows="7" placeholder='Your Message' required></textarea>
           <button type='submit' className='btn btn-primary'>Send Message</button>
         </form>
